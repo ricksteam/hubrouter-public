1c6ba5f1be9bf47ea7924487f438be994efb449e
B Ricks <bricks@unomaha.edu>
2019-07-23 06:29:01 -0500

    Add comments to the functions

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
+/*
+MIT License
+
+Copyright (c) 2019 B. Ricks
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
 const express = require("express");
 const routes = require("express").Router();
 const axios = require("axios");
-//const redis = require("redis");
 
 const bodyParser = require("body-parser");
 
@@ -13,19 +36,34 @@ app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 const _ = require("lodash");
 let stringify = require("json-stringify-safe");
 
-/*const redisClient = redis.createClient();
-redisClient.on("connect", function() { 
-  //console.log("Connected to redis");
-});*/
-
+/**
+ * Convert a string that is encoded in base 64 into a string that is encoded is ascii
+ * @param {String} base64 A base64 encoded string
+ * @return {String} A ascii encoded string
+ */
 function fromBase64ToAscii(base64) {
   return new Buffer(base64, "base64").toString();
 }
 
+/**
+ * Convert a string that is encoded in ascii into a string that is encoded in base64
+ * @param {String} ascii An ascii encoded string
+ * @return {String} A base 64 encode string
+ */
 function fromAsciiToBase64(ascii) {
   return new Buffer(ascii).toString("base64");
 }
 
+/**
+ * Save an ascii encoded file to a github using the repo/contents api.  
+ * @param {String} owner The owner/organization in which the file will go
+ * @param {String} repo The repository in which the file will go
+ * @param {String} filename The name of the file to save
+ * @param {String} contents The ascii-encoded file to save
+ * @param {String} token The acess token to pass to the github api
+ * @param {String} message The commit message to attach to the commit
+ * @return {Promise} The resulting promise from the commit network call.
+ */
 function saveToGithub(owner, repo, filename, contents, token, message) {
   let realMessage = message ? message : "Created via file API";
   let call = `https://api.github.com/repos/${owner}/${repo}/contents/${filename}?access_token=${token}`;
@@ -36,6 +74,16 @@ function saveToGithub(owner, repo, filename, contents, token, message) {
   });
 }
 
+/**
+ * Update a file to a github wusing the repo/contents api with an ascii encoded file.  
+ * @param {String} owner The owner/organization in which the file will go
+ * @param {String} repo The repository in which the file will go
+ * @param {String} filename The name of the file to save
+ * @param {String} contents The ascii-encoded file to save
+ * @param {String} token The acess token to pass to the github api
+ * @param {String} message The commit message to attach to the commit
+ * @return {Promise} The resulting promise from the commit network call.
+ */
 function updateToGithub(owner, repo, filename, contents, token, sha, message) {
   let realMessage = message ? message : "Created via file API";
   return axios.put(
@@ -48,11 +96,33 @@ function updateToGithub(owner, repo, filename, contents, token, sha, message) {
   );
 }
 
+/**
+ * Get the contents of a file in github using the repos/contents api. 
+ * This function has tighter file size constraints than the function getGithubWithSHA. 
+ * Therefore, it is best to avoid this function when possible.
+ * @param {String} owner The owner/organization where the file resides
+ * @param {String} repo The repository where the file resides
+ * @param {String} filename The name of the file to retrieve
+ * @param {String} token The acess token to pass to the github api
+ * @return {Promise} The resulting promise from the get network call.
+ */
+
 function getInGithub(owner, repo, filename, token) {
   let call = `https://api.github.com/repos/${owner}/${repo}/contents/${filename}?access_token=${token}`;
   //console.log(call);
   return axios.get(call);
 }
+
+/**
+ * Get the contents of a file in github using the blobs api. 
+ * Thin function can retrieve larger files than the function getInGithub. 
+ * Therefore, it is the prefered function to use.
+ * @param {String} owner The owner/organization where the file resides
+ * @param {String} repo The repository where the file resides
+ * @param {String} sha The sha of the file to retrieve
+ * @param {String} token The acess token to pass to the github api
+ * @return {Promise} The resulting promise from the get network call.
+ */
 
 function getInGithubWithSHA(owner, repo, sha, token) {
   console.log("Doing a sha call " + sha);
@@ -61,6 +131,16 @@ function getInGithubWithSHA(owner, repo, sha, token) {
   return axios.get(call);
 }
 
+/**
+ * Remove a file from Github.
+ * @param {String} owner The owner/organization in which the file resides
+ * @param {String} repo  The repository in which the file resides
+ * @param {String} filename The name of the file to remove
+ * @param {String} token The access token granting permission to do the removal
+ * @param {String} sha The sha of the file to remove
+ * @param {String} message The commit message
+ * @returns {Promise} The resulting promise from the delete network call.
+ */
 function deleteToGithub(owner, repo, filename, token, sha, message) {
   let realMessage = message ? message : "Created via file API";
   let call = `https://api.github.com/repos/${owner}/${repo}/contents/${filename}?access_token=${token}`;
@@ -72,6 +152,13 @@ function deleteToGithub(owner, repo, filename, token, sha, message) {
   });
 }
 
+/**
+ * Github returns a lot of information about an file and gives the content as base64-encoded.
+ * This function simplifies that object by only returning the keys that are relevant 
+ * and converting the base64 content to ascii-encoded content.
+ * @param {Object} githubFileObject The file object returned from github to be simplified
+ * @returns A file object with fewer keys and ascii-encoded content.
+ */
 function mapFile(githubFileObject) {
   let toReturn = {};
   let keysToKeep = ["type", "name", "sha", "path"];
@@ -88,6 +175,12 @@ function mapFile(githubFileObject) {
   return toReturn;
 }
 
+/**
+ * Github returns a lot of information about a directory.
+ * The function returns an object with only the keys of interest
+ * @param {Object} githubDirectoryObject A directory object received from a call to the Github api.
+ * @returns A directory object with only the relevant keys
+ */
 function mapDirectory(githubDirectoryObject) {
   let toReturn = {};
   let keysToKeep = ["type", "name", "sha", "path"];
@@ -100,6 +193,12 @@ function mapDirectory(githubDirectoryObject) {
   return toReturn;
 }
 
+/**
+ * Recursively create a flat map of the files in a directory, ignoring readme files if requisted
+ * @param {Array} fileLikeArray An array of file descriptors as returned by Github when querying the contents of a directory
+ * @param {Boolean} includeReadme True if the result should include the readme files, false otherwise
+ * @returns {Array} A flat-mapped array of just file descriptors that includes readme files if requested.
+ */
 function mapFileLikeObjects(fileLikeArray, includeReadme) {
   let toReturn = [];
 
@@ -116,6 +215,28 @@ function mapFileLikeObjects(fileLikeArray, includeReadme) {
   return toReturn;
 }
 
+/**
+ * Retrieve a content from github and return it as ascii-enncoded text
+ * If the user includes a sha (preferred), then the content is retrieved using a sha.
+ * In this case, the path is ignored.
+ * Otherwise, the content is retreived using the path, in which case the sha is ignored.
+ * 
+ * Once the result come back from Github, the result is analyzed to see if it is a file descriptor
+ * Or a directory descriptor (i.e. an array of file descriptors).
+ * If it is a file descriptor, the function immediately continues.
+ * If not, the function recursively flat maps the directory including the readme as requested
+ * In either case, the resulting file descriptor or array of file descriputors as "returned" 
+ * using the express response variable provided.
+ * 
+ * An errors that are caught and retured using the express response variable provided.
+ * @param {String} res Express middleway response variable used to send the results of this function back to the caller
+ * @param {String} org The owner/organization in which the file resides
+ * @param {String} repo The repository in which the file resides
+ * @param {String} path The path of the file. *Note* This parameter is ignored if the sha parameter is provided.
+ * @param {String} accessToken The access token used to grant access to the file
+ * @param {String} sha The SHA of the file. *Note* This parameter is optional, though proferred. If it is not provided, the path parameter must be provided.
+ * @param {Boolean} includeReadme Whether or not to include readme files which requesting the file descriptors of a directory. *Note* This is optional when reuesting only a file.
+ */
 function retrieveFromGithub(res, org, repo, path, accessToken, sha, includeReadme) {
   let promise;
   if (sha)
@@ -124,9 +245,7 @@ function retrieveFromGithub(res, org, repo, path, accessToken, sha, includeReadm
     promise = getInGithub(org, repo, path, accessToken);
   promise
     .then(result => {
-      //console.log("Parsing result");
-
-      //If the result is a directory, result.data is an array
+       //If the result is a directory, result.data is an array
       if (Array.isArray(result.data)) {
         res.send(mapFileLikeObjects(result.data, includeReadme));
       } else {
@@ -144,6 +263,16 @@ function retrieveFromGithub(res, org, repo, path, accessToken, sha, includeReadm
     });
 }
 
+/**
+ * Route to request a resource from Github. If it is a file, the ascii-encoded contents are returened
+ * Expected values in the body of the post:
+ * org: The owner/organization in which the file/directory resides
+ * repo: The repository in which the file resides
+ * accessToken: The access token that grants access for the operation
+ * path: The path of the file to retrieve. *Note* This is ignored if the sha is provided (proferred).
+ * sha: The sha of the file to retrieve. *Note* This is optional if the path parameter is provided.
+ * includeReadme: If a directory is requested, should the readme file descriptors be returned?
+ */
 routes.post("/crud/retrieve/:org/:repo", (req, res) => {
   let accessToken = req.res.locals.accessToken;
   let repo = req.params.repo;
@@ -151,26 +280,13 @@ routes.post("/crud/retrieve/:org/:repo", (req, res) => {
   let sha = req.body.sha;
   let org = req.params.org;
   let includeReadme = req.body.includeReadme;
-  //console.log(`Got db ${repo} ${path}`);
-
-  /*if (sha != undefined) {
-    //console.log("Checking redis file entry for " + sha);
-    redisClient.hgetall(sha, function(err, reply) {
-      if (reply != null) {
-        //console.log("Found entry for " + sha);
-        res.send(reply);
-      } else {
-        //console.log("No entry found. Polling GitHub...");
-        retrieveFromGithub(res, org, repo, path, accessToken, includeReadme);
-      }
-    });
-  } else {
-    console.log("No sha in request body, skipping redis cache check...");
-    retrieveFromGithub(res, org, repo, path, accessToken, includeReadme);
-  }*/
+  
   retrieveFromGithub(res, org, repo, path, accessToken, sha, includeReadme);
 });
 
+/**
+ * @deprecated
+ */
 //Get everything in a repo, up to 1,000 items
 routes.get("/crud/retrieveAll/:org/:repo", (req, res) => {
   let accessToken = req.res.locals.accessToken;
@@ -194,6 +310,16 @@ routes.get("/crud/retrieveAll/:org/:repo", (req, res) => {
     });
 });
 
+/**
+ * Create a resource in github from an ascii-encoded file
+ * This route expects the following values in the post body:
+ * org: The owner/organization in which the file/directory will reside
+ * repo: The repository in which the file will reside
+ * accessToken: The access token that grants access for the operation
+ * path: The path of the file. 
+ * message: The commit message.
+ * content: The ascii-encoded content of the file
+  */
 routes.post("/crud/create/:org/:repo", (req, res) => {
   let accessToken = req.res.locals.accessToken;
   let repo = req.params.repo;
@@ -217,6 +343,17 @@ routes.post("/crud/create/:org/:repo", (req, res) => {
     });
 });
 
+/**
+ * Update a resource in github from an ascii-encoded file
+ * This route expects the following values in the post body:
+ * org: The owner/organization in which the file/directory  resides
+ * repo: The repository in which the file resides
+ * accessToken: The access token that grants access for the operation
+ * path: The path of the file.
+ * sha: The sha of the file 
+ * message: The commit message.
+ * content: The ascii-encoded content of the file
+  */
 routes.post("/crud/update/:org/:repo", (req, res) => {
   let accessToken = req.res.locals.accessToken;
   let repo = req.params.repo;
@@ -249,6 +386,17 @@ routes.post("/crud/update/:org/:repo", (req, res) => {
     });
 });
 
+/**
+ * Route to remove a resource from Github
+ * Expected values in the body of the post:
+ * org: The owner/organization in which the file/directory resides
+ * repo: The repository in which the file resides
+ * accessToken: The access token that grants access for the operation
+ * path: The path of the file to remove.
+ * sha: The sha of the file to remove. 
+ * message: The commit message to accompany the removal
+ 
+ */
 routes.post("/crud/delete/:org/:repo", (req, res) => {
   let accessToken = req.res.locals.accessToken;
   let repo = req.params.repo;
@@ -272,6 +420,12 @@ routes.post("/crud/delete/:org/:repo", (req, res) => {
     });
 });
 
+/**
+ * Get a list of the repositories in an organization
+ * Expected values ithe body of this post are:
+ * org: The owner/organiation that holds the repositories
+ * accessToken: The access token used for the query
+ */
 routes.get("/repos/:org", (req, res) => {
   let accessToken = req.res.locals.accessToken;
   let org = req.params.org;
